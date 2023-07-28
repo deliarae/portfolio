1345bbf5f2f81562dd9e4830ee52c3ecc6015984
Delia Rauftlin <103967101+deliarae@users.noreply.github.com>
2023-07-28 18:07:30 +0100

    switch is now functional

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let darkModeBtn = document.getElementById("dark-mode-switch")
+// let darkModeBtn = document.getElementById("dark-mode-switch")
 
-darkModeBtn.addEventListener('click', function changeColor(){
-    let body = document.body
-    body.classList.toggle("dark-mode");
-})
+// darkModeBtn.addEventListener('click', function changeColor(){
+//     let body = document.body
+//     body.classList.toggle("dark-mode");
+// })
+
+// ----------------- theme switch ------------------ //
+const switchTheme = () => {
+    const rootElement = document.documentElement
+    let dataTheme = rootElement.getAttribute('data-theme'), 
+        newTheme
+
+    newTheme = (dataTheme === 'dark') ? 'light' : 'dark'
+
+    rootElement.setAttribute('data-theme', newTheme)
+}
+
+document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
+
+
+// -----------------  ------------------ //
