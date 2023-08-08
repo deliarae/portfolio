9f225105272e483bdd32f2f89d8a3461a737875e
Delia Rauftlin <103967101+deliarae@users.noreply.github.com>
2023-08-08 13:30:09 +0100

    made some changes to projects

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// let darkModeBtn = document.getElementById("dark-mode-switch")
 
-// darkModeBtn.addEventListener('click', function changeColor(){
-//     let body = document.body
-//     body.classList.toggle("dark-mode");
-// })
 
 // ----------------- theme switch ------------------ //
 const switchTheme = () => {
